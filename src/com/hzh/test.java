ef06f23d56802eb95835f5e8f49d325b5d0cab9e
chinawudi <2117009169@qq.com>
2020-12-24 15:26:03 +0800

    123

---
+++ src/com/hzh/test.java
@@ -0,0 +1,13 @@
+package com.hzh;
+
+/**
+ * @author hzh
+ * @version 1.0
+ * @descripton:
+ * @date 2020/12/24 17:05
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("cs");
+    }
+}
